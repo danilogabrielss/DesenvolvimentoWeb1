66ffcdec0d1a45b47ed6ca4c9b55ce899f1f95cb
Danilo Gabriel <59902770+danilogabrielss@users.noreply.github.com>
2020-09-11 21:34:30 -0300

    exercicios anteriores

---
+++ aula_validacao_formulario/recebe.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo $_POST['firstname'];
+    echo "</br>";
+    echo $_POST['lastname'];
+    echo "</br>";
+    echo $_POST['birthday'];
+    echo "</br>";
+    echo $_POST['email'];
+    echo "</br>";
+    echo $_POST['idade'];
+    echo "</br>";
+    echo $_POST['estado'];
+    echo "</br>";
+    echo $_POST['browser'];
+    echo "</br>";
+    echo $_POST['carros'];
+    echo "</br>";
+    echo $_POST['placa'];
+    echo "</br>";
+    ?>
+</body>
+
+</html>
---
+++ formularioespecial/recebe-formulario.php
@@ -0,0 +1,30 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+</head>
+
+<body>
+
+<?php 
+
+echo $_POST['nome']."<br>";
+echo $_POST['email']."<br>";
+echo $_POST['nascimento']."<br>";
+echo $_POST['telefone']."<br>";
+echo $_POST['site']."<br>";
+echo $_POST['nascimeto']."<br>";
+echo $_POST['temperatura']."<br>";
+echo $_POST['hora']."<br>";
+echo $_POST['senha']."<br>";
+echo $_POST['disciplina']."<br>";
+echo $_POST['cor_favorita']."<br>";
+echo $_POST['mensagem']."<br>";
+echo $_POST['receber_email']."<br>";
+echo $_POST['email_parceiros']."<br>";
+
+?>
+
+</body>
+</html>
